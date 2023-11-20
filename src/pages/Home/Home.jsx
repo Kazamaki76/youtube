323a3e1ee78b3e02110b01ff7609c0a47ae3e5c0
kazamaki76@gmail.com <kazamaki76@gmail.com>
2023-11-21 02:28:23 +0700

    Categories done

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
-import Slider from "../../components/slider/Slider"
-import "./Home.scss"
+import React from "react";
+import Slider from "../../components/slider/Slider";
+import "./Home.scss";
+import FeatureItems from "../../components/FeatureItems/FeatureItems"
+import Categories from "../../components/Categories/Categories";
 
 function Home() {
   return (
-    <div className=''> <Slider/></div>
-  )
+    <div className="">
+      {" "}
+      <Slider />
+      <FeatureItems type="feature"/>
+      <Categories/>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
