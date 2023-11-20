04b4bfd97ea44dfa350d603e21dbff3f4029ac00
kazamaki76@gmail.com <kazamaki76@gmail.com>
2023-11-21 00:05:47 +0700

    slider done

--- src/components/slider/Slider.jsx
+++ src/components/slider/Slider.jsx
@@ -1,25 +1,33 @@
 import React from "react";
 import KeyboardDoubleArrowLeftIcon from '@mui/icons-material/KeyboardDoubleArrowLeft';
 import KeyboardDoubleArrowRightIcon from '@mui/icons-material/KeyboardDoubleArrowRight';
-
+import "./Slider.scss"
+import { useState } from "react";
 
 const Slider = () => {
+  const [currentSlide , setCurrentSlide] = useState(0)
   const data = [
     "https://www.yugioh-card.com/eu/wp-content/uploads/2022/10/New-to-YGO-Hero-Banner-Card-EN.webp",
     "https://farm9.staticflickr.com/8500/8312661685_23447519b0_o.jpg",
 
   ];
+  const previousSlide = () => {
+    setCurrentSlide(currentSlide === 0? 2 :(prev ) => prev-1)
+  }
+  const nextSlide =() => {
+    setCurrentSlide(currentSlide === 2? 0 :(prev ) => prev+1)
+  }
   return (
-    <div>
+    <div className="slider">
       {" "}
-      <div className="container">
+       <div className="container" style={{transform:`translateX(-${currentSlide * 100}vw)`}}>
         <img src={data[0]}></img>
         <img src={data[1]}></img>
         <img src={data[0]}></img>
       </div>{" "}
       <div className="icons">
-        <div className="icon"> <KeyboardDoubleArrowLeftIcon/> </div>
-        <div className="icon"> <KeyboardDoubleArrowRightIcon/> </div>
+        <div className="icon" onClick={previousSlide}> <KeyboardDoubleArrowLeftIcon/> </div>
+        <div className="icon" onClick={nextSlide}> <KeyboardDoubleArrowRightIcon/> </div>
      
       </div>
     </div>
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Slider from "../../components/slider/Slider"
+import "./Home.scss"
 
 function Home() {
   return (
